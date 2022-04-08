6efac0c1f53707c263c8ca70cc01e550e80ea336
Chris Lin <clin@idgenterprise.com>
2022-04-07 18:22:47 -0700

    change POSTGRES_DATABASE_URL

--- database.py
+++ database.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = 'postgres://qxlwehffsjoldk:50d2815916dbc44c0b33b307d73cc136ee3b87e9c95b861d5dbec8fde8352f24@ec2-52-3-60-53.compute-1.amazonaws.com:5432/d25t8boh3a6en5'
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")  # or other relevant config var
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
