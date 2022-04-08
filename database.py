5c4c66f3e38dd7e73cd802d4ff66c9b36b7db88f
Chris Lin <clin@idgenterprise.com>
2022-04-07 18:25:43 -0700

    change POSTGRES_DATABASE_URL

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")  # or other relevant config var
